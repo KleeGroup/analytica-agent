ee208c58eb82c5eb30194a376efdddbb59187689
NPi2Loup <npiedeloup@gmail.com>
2013-01-29 15:00:39 +0100

    pour test

---
+++ src/main/java/com/kleegroup/analyticaimpl/agent/plugins/net/direct/DirectNetPlugin.java
@@ -0,0 +1,36 @@
+package com.kleegroup.analyticaimpl.agent.plugins.net.direct;
+
+import javax.inject.Inject;
+
+import kasper.kernel.util.Assertion;
+
+import com.kleegroup.analytica.core.KProcess;
+import com.kleegroup.analytica.server.ServerManager;
+import com.kleegroup.analyticaimpl.agent.plugins.net.NetPlugin;
+
+/**
+ * Direct call to Analytica Server.
+ * No buffering. Design for tests purpose.
+ * @author npiedeloup
+ * @version $Id: DirectNetPlugin.java,v 1.4 2012/10/16 08:30:56 pchretien Exp $
+ */
+public final class DirectNetPlugin implements NetPlugin {
+	private final ServerManager serverManager;
+
+	/**
+	 * Constructor.
+	 * @param serverManager Analytica ServerManager
+	 */
+	@Inject
+	public DirectNetPlugin(final ServerManager serverManager) {
+		Assertion.notNull(serverManager);
+		//---------------------------------------------------------------------
+		this.serverManager = serverManager;
+
+	}
+
+	/** {@inheritDoc} */
+	public void add(final KProcess process) {
+		serverManager.push(process);
+	}
+}
