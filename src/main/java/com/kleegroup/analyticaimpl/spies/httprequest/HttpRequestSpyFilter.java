5ad9a4afb9d5e732c594049e81cb126f5d4bdc0c
NPi2Loup <npiedeloup@gmail.com>
2013-01-31 10:58:17 +0100

    Reprise de AnalyticsUIFilter

--- src/main/java/com/kleegroup/analyticaimpl/spies/httprequest/HttpRequestSpyFilter.java
+++ src/main/java/com/kleegroup/analyticaimpl/spies/httprequest/HttpRequestSpyFilter.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kasper.kernel.Home;
-import kasper.kernel.exception.KRuntimeException;
 import kasper.kernel.exception.KUserException;
 
 import org.apache.log4j.Logger;
@@ -22,26 +21,21 @@ import com.kleegroup.analytica.agent.AgentManager;
 /**
  * Filtre de servlet (javax.servlet.Filter).
  * Il permet d'instrumenter les pages avec appels, durées, taille des flux d'entrée et de sortie.
- *
+ * TODO : redondant avec kasperimpl.jsf.servlet.analytics.AnalyticsUIFilter
  * @author pchretien, npiedeloup 
  * @version $Id: UIFilter.java,v 1.7 2010/11/16 10:36:54 pchretien Exp $
  */
 public final class HttpRequestSpyFilter implements Filter {
 
-	private static final String PT_PAGE = "PAGE";
-	private static final String ME_USER_EXCEPTION_PCT = "USER_EXCEPTION_PCT";
-	private static final String ME_SYSTEM_EXCEPTION_PCT = "SYSTEM_EXCEPTION_PCT";
-	private static final String ME_HTML_SIZE = "HTML_SIZE";
+	private static final String PT_REQUEST = "REQUEST";
+	private static final String ME_RESPONSE_LENGTH = "RESPONSE_LENGTH";
+	private static final String ME_USER_EXCEPTION_PCT = "USER_EXCEPTION_COUNT";
+	private static final String ME_OTHER_EXCEPTION_PCT = "OTHER_EXCEPTION_COUNT";
 
 	/**
 	 * Mécanisme de log racine
 	 */
 	private final Logger generalLog = Logger.getRootLogger();
-
-	/**
-	 * Mécanisme de log utilisé pour les performances
-	 */
-	private final Logger performanceLog = Logger.getLogger("Performance");
 
 	/** {@inheritDoc} */
 	public void init(final FilterConfig filterConfig) {
@@ -73,45 +67,37 @@ public final class HttpRequestSpyFilter implements Filter {
 	 */
 	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 		if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
-			filter((HttpServletRequest) request, new HttpRequestSpyServletResponseWrapper((HttpServletResponse) response), chain, getAgentManager());
+			filter((HttpServletRequest) request, (HttpServletResponse) response, chain);
 		} else {
 			chain.doFilter(request, response);
 		}
 	}
 
-	private void filter(final HttpServletRequest httpRequest, final HttpRequestSpyServletResponseWrapper wrappedResponse, final FilterChain chain, final AgentManager agentManager) throws IOException, ServletException {
+	private void filter(final HttpServletRequest httpRequest, final HttpServletResponse httpResponse, final FilterChain chain) throws IOException, ServletException {
+		final AgentManager agentManager = getAgentManager();
 		final long begin = System.currentTimeMillis();
 		boolean ok = false;
 		//Démarrage de la requête.
-		agentManager.startProcess(PT_PAGE, getRequestName(httpRequest));
+		final String processName = getProcessName(httpRequest);
+		agentManager.startProcess(PT_REQUEST, processName);
 		logRequestStart(httpRequest);
-
+		final HttpRequestSpyServletResponseWrapper wrappedResponse = new HttpRequestSpyServletResponseWrapper(httpResponse);
 		try {
-			chain.doFilter(httpRequest, wrappedResponse);
+			chain.doFilter(httpRequest, httpResponse);
+			agentManager.setMeasure(ME_RESPONSE_LENGTH, wrappedResponse.getDataLength());
 			ok = true;
 		} catch (final ServletException servletException) {
+			//Permet de compter les erreurs par type.
 			if (isUserException(servletException)) {
 				agentManager.setMeasure(ME_USER_EXCEPTION_PCT, 100);
 			} else {
-				agentManager.setMeasure(ME_SYSTEM_EXCEPTION_PCT, 100);
+				agentManager.setMeasure(ME_OTHER_EXCEPTION_PCT, 100);
 			}
 			logRequestException(servletException);
 			throw servletException;
 		} finally {
-			final long requestTime = System.currentTimeMillis() - begin;
-			// Fin de l'instrumentation générique de la servlet
-			// Ajout de compteurs spécifiques au projet
-			String requestSimpleName = httpRequest.getPathInfo();
-			if (requestSimpleName == null) {
-				requestSimpleName = httpRequest.getServletPath();
-			}
-			if (wrappedResponse.getDataLength() != 0) {
-				//en cas d'erreur on n'a pas la taille, il ne faut pas la compter
-				agentManager.setMeasure(ME_HTML_SIZE, wrappedResponse.getDataLength());
-			}
 			agentManager.stopProcess();
-
-			logRequestFinish(httpRequest, requestTime, ok);
+			logRequestFinish(httpRequest, System.currentTimeMillis() - begin, ok);
 		}
 	}
 
@@ -119,14 +105,10 @@ public final class HttpRequestSpyFilter implements Filter {
 
 	private static boolean isUserException(final Throwable throwable) {
 		Throwable e = throwable;
-		Boolean userException = null;
 
 		while (e != null) {
-			if (userException == null && e instanceof KUserException) {
-				userException = Boolean.TRUE;
-			}
-			if (userException == null && e instanceof KRuntimeException) {
-				userException = Boolean.FALSE;
+			if (e instanceof KUserException) {
+				return true;
 			}
 
 			Throwable t = e.getCause();
@@ -138,7 +120,63 @@ public final class HttpRequestSpyFilter implements Filter {
 			}
 			e = t;
 		}
-		return Boolean.TRUE.equals(userException);
+		return false;
+	}
+
+	/**
+	 * Retourne le nom du type de la requête.
+	 *
+	 * @param request Requête HTTP
+	 * @return Nom du dossier dans lequel sont classés les résultats
+	 */
+	private String getProcessName(final HttpServletRequest request) {
+		final StringBuilder requestName = new StringBuilder(request.getMethod());
+		requestName.append(" ");
+		final String pathInfo = request.getPathInfo();
+		if (pathInfo != null) {
+			requestName.append(pathInfo);
+		} else {
+			final String servletPath = request.getServletPath();
+			requestName.append(servletPath.substring(0, servletPath.indexOf('/', 1))).append("/*");
+		}
+		return requestName.toString();
+	}
+
+	private static AgentManager getAgentManager() {
+		//Le Filter étant un filtre de servlet, il ne supporte pas l'injection de dépendance via le contructeur
+		return Home.getContainer().getManager(AgentManager.class);
+	}
+
+	//-------------------------------------------------------------------------
+
+	private void logRequestStart(final HttpServletRequest request) {
+		if (generalLog.isTraceEnabled()) {
+			generalLog.trace("[Start] " + getRequestNameFull(request));
+		}
+	}
+
+	private void logRequestFinish(final HttpServletRequest httpRequest, final long requestTime, final boolean success) {
+		final String requestName = getProcessName(httpRequest);
+		if (success) {
+			if (generalLog.isTraceEnabled()) {
+				generalLog.trace("[Finish] " + requestName + " (" + getRequestNameFull(httpRequest) + ") reussie en  ( " + requestTime + ") ms");
+			} else if (generalLog.isInfoEnabled()) {
+				generalLog.info("[Finish] " + requestName + " reussie en  ( " + requestTime + ") ms");
+			}
+		} else {
+			//Echec
+			if (requestName == null) {
+				generalLog.warn("[Finish] -NoRequestName- interrompue après ( " + requestTime + ") ms");
+			} else {
+				generalLog.warn("[Finish] " + requestName + " interrompue après ( " + requestTime + ") ms");
+			}
+		}
+	}
+
+	private void logRequestException(final Exception exception) {
+		// e.toString()) affiche la classe du message et le message lui même.
+		// Une KUser non proprement géré est volontairement loggée en erreur.
+		generalLog.error(exception.toString(), exception);
 	}
 
 	private static String getRequestNameFull(final HttpServletRequest request) {
@@ -154,69 +192,5 @@ public final class HttpRequestSpyFilter implements Filter {
 		}
 		requestName.append(')');
 		return requestName.toString();
-	}
-
-	/**
-	 * Retourne le nom du type de la requête.
-	 *
-	 * @param request Requête HTTP
-	 * @return Nom du dossier dans lequel sont classés les résultats
-	 */
-	private String getRequestName(final HttpServletRequest request) {
-		final StringBuilder requestName = new StringBuilder(request.getMethod());
-		requestName.append(" ");
-		final String pathInfo = request.getPathInfo();
-		if (pathInfo != null) {
-			requestName.append(pathInfo);
-		} else {
-			final String servletPath = request.getServletPath();
-			requestName.append(servletPath.substring(0, servletPath.indexOf('/', 1))).append("/*");
-		}
-		return requestName.toString();
-	}
-
-	//-------------------------------------------------------------------------
-
-	private void logRequestStart(final HttpServletRequest request) {
-		if (generalLog.isTraceEnabled()) {
-			generalLog.trace("[Start] " + getRequestNameFull(request));
-		}
-	}
-
-	// IsoFonctionnel existant
-	// @Todo revoir implémentation
-	private void logPerformance(final String controllerName, final long elapsedTime) {
-		if (performanceLog.isInfoEnabled()) {
-			performanceLog.info(">> Request : " + controllerName + " : time = " + elapsedTime);
-		}
-	}
-
-	private void logRequestFinish(final HttpServletRequest httpRequest, final long requestTime, final boolean success) {
-		final String requestName = getRequestName(httpRequest);
-		if (success) {
-			if (generalLog.isTraceEnabled()) {
-				generalLog.trace("[Finish] " + requestName + " (" + getRequestNameFull(httpRequest) + ") reussie en  ( " + requestTime + ") ms");
-			} else if (generalLog.isInfoEnabled() && !performanceLog.isInfoEnabled()) {
-				generalLog.info("[Finish] " + requestName + " reussie en  ( " + requestTime + ") ms");
-			}
-			logPerformance(requestName, requestTime);
-		} else {
-			//Echec
-			if (requestName == null) {
-				generalLog.warn("[Finish] -NotFound- interrompue après ( " + requestTime + ") ms");
-			} else {
-				generalLog.warn("[Finish] " + requestName + " interrompue après ( " + requestTime + ") ms");
-			}
-		}
-	}
-
-	private void logRequestException(final Exception exception) {
-		// e.toString()) affiche la classe du message et le message lui même.
-		// Une KUser non proprement géré est volontairement loggée en erreur.
-		generalLog.error(exception.toString(), exception);
-	}
-
-	private static AgentManager getAgentManager() {
-		return Home.getContainer().getManager(AgentManager.class);
 	}
 }
