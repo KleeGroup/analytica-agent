65a6a4896ba3add3412a25077079981e68b8d37c
NPi2Loup <npiedeloup@gmail.com>
2013-10-03 18:01:56 +0200

    client-embedded

---
+++ analytica-client-embedded/src/main/java/com/kleegroup/analyticaimpl/agent/plugins/net/direct/DirectNetPlugin.java
@@ -0,0 +1,46 @@
+package com.kleegroup.analyticaimpl.agent.plugins.net.direct;
+
+import javax.inject.Inject;
+
+import vertigo.kernel.lang.Assertion;
+
+import com.kleegroup.analytica.core.KProcess;
+import com.kleegroup.analytica.server.ServerManager;
+import com.kleegroup.analyticaimpl.agent.plugins.net.NetPlugin;
+
+/**
+ * Direct call to Analytica Server.
+ * No buffering. Design for tests purpose.
+ * @author npiedeloup
+ * @version $Id: DirectNetPlugin.java,v 1.4 2012/10/16 08:30:56 pchretien Exp $
+ */
+public final class DirectNetPlugin implements NetPlugin {
+	private final ServerManager serverManager;
+
+	/**
+	 * Constructor.
+	 * @param serverManager Analytica ServerManager
+	 */
+	@Inject
+	public DirectNetPlugin(final ServerManager serverManager) {
+		Assertion.checkNotNull(serverManager);
+		//---------------------------------------------------------------------
+		this.serverManager = serverManager;
+
+	}
+
+	/** {@inheritDoc} */
+	public void add(final KProcess process) {
+		serverManager.push(process);
+	}
+
+	/** {@inheritDoc} */
+	public void start() {
+		//rien
+	}
+
+	/** {@inheritDoc} */
+	public void stop() {
+		//rien
+	}
+}
